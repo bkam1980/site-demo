7c2e4b5333c6abd9054ff6b1ea94250adb593af8
bakary <b.kamara1980@gmail.com>
2018-04-06 11:09:55 +0200

    ajout css

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
 <html>
 	<head>
 		<title>Site démo</title>
+		<style>
+			h1{
+				position: relative;
+				top: 50%;
+				left: 50%;
+				font-size: 20px;
+				color: aqua;			
+			}		
+		</style>
 	</head>
 	<body>
 		<h1>Bonjour !</h1>
